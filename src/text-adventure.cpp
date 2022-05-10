cdb94f67feb7c8971beb50f262047072d706c4f8
Timothy <Rorie.TimothyJr@gmail.com>
2022-05-10 02:35:54 -0400

    Initial Commit

---
+++ src/text-adventure.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <cstring>
+#include <cmath>
+
+int main () {
+  std::string msg_welcome = "Welcome newly registered Adventurer. What is your name?";
+
+  std::cout << msg_welcome << std::endl;
+}
